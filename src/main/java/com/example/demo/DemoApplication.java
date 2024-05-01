dc9d53b54a6f5465d8651c03f2088b4fe611f1b5
김태호 <>
2024-05-01 23:34:40 +0900

    1. Controller에 데이터 crud 기능이 있던것을 분리 진행 2. @Entity 추가 3. Repository 추가(h2 활용)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,17 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,8 +29,18 @@ public class DemoApplication {
 //애플리케이션을 설정한 다음, '초기 컨텍스트'를 생성하고 스프링 부트 애플리케이션을 실행한다.
 //최상위 어노테이션 @SpringBootApplication 과 한줄의 코드로 실행한다.
 
+
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
+
+//JPA를 사용해 DB에 데이터를 생성할 때는 기본 생성자no-argument가 필요하다.
+//기본 생성자를 사용하려면 모든 멤버 변수를 final이 아닌 변경 가능으로 만들어야 한다.
+@Entity
+@Getter
+@Setter
 class Coffee{
-	private final String id;
+	@Id
+	private String id;
 	private String name;
 
 	public Coffee(){
@@ -38,55 +56,42 @@ class Coffee{
 		this(UUID.randomUUID().toString(), name);
 	}
 
-	public String getId(){
-		return id;
-	}
-
-	public String getName(){
-		return name;
-	}
-
-	public void setName(String name){
-		this.name = name;
-	}
 }
 
+//스프링 프레임워크 4.3 이전 버전에서는 매개변수가 autowire/주입될 스프링 빈일 때 해당 메서드에 @Autowired 어노테이션을 표시
+//4.3 이후 버전 부터 단일 생성자 클래스는 autowire되는 매개변수를 나타내기 위한 어노테이션이 필요 없다.
 @RestController
 @RequestMapping("/coffees")
 class RestApiDemoController{
-	private List<Coffee> coffees = new ArrayList<>();
+	private final CoffeeRepository coffeeRepository;
 
-	public RestApiDemoController(){
-		coffees.addAll(List.of(
+	public RestApiDemoController(CoffeeRepository coffeeRepository){
+        this.coffeeRepository = coffeeRepository;
+        this.coffeeRepository.saveAll(List.of(
 				new Coffee("Cafe Cereza"),
 				new Coffee("Cafe Ganador"),
 				new Coffee("Cafe Lareno"),
 				new Coffee("Cafe Tres Pontas")
 		));
+
+		//coffees.addAll();
 	}
 
 	@GetMapping
 	Iterable<Coffee> getCoffees(){
-		return coffees;
+		return coffeeRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
-	Optional<Coffee> getCoffees(@PathVariable("id") String id){
-		System.out.println("id = " + id);
-		for(Coffee c: coffees){
-			if(c.getId().equals(id)){
-				return Optional.of(c);
-			}
-		}
-		return Optional.empty();
+	Optional<Coffee> getCoffeeById(@PathVariable("id") String id){
+		return coffeeRepository.findById(id);
 	}
 
 	//여기서 신기한건 Coffee 객체는 스프링 부트에 의해 언마샬링(기본값은 JSON)되어 요청한 애플리케이션이나 서비스로 반환
 	//상태 코드 사용을 권장
 	@PostMapping
 	Coffee postCoffee(@RequestBody Coffee coffee){
-		coffees.add(coffee);
-		return coffee;
+		return coffeeRepository.save(coffee);
 	}
 	// * 마샬링 : 객체나 특정 형태의 데이터를 저장 및 전송 가능한 데이터 형태로 변환하는 과정
 	// * 언마샬링 : 변환했던 데이터를 원래대로 복구하는 과정
@@ -96,25 +101,33 @@ class RestApiDemoController{
 	//상태 코드는 필수!
 	@PutMapping("/{id}")
 	ResponseEntity<Coffee> putCoffee(@PathVariable("id") String id, @RequestBody Coffee coffee){
-		int coffeeIndex = -1;
-
-		for(Coffee c: coffees){
-			if(c.getId().equals(id)){
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
-
-		return (coffeeIndex == -1) ?
-				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-				new ResponseEntity<>(coffee, HttpStatus.OK);
+		return (coffeeRepository.existsById(id))
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
 	}
 
 
 	//상태 코드 사용을 권장
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id){
-		//removeIf는 Predicate 값을 받는다. 즉 목록에 제거할 커피가 존재시 참
-		coffees.removeIf(c -> c.getId().equals(id));
+		coffeeRepository.deleteById(id);
+	}
+}
+
+@Component
+class DataLoader{
+	private final CoffeeRepository coffeeRepository;
+	public DataLoader(CoffeeRepository coffeeRepository){
+		this.coffeeRepository = coffeeRepository;
+	}
+
+	@PostConstruct
+	private void loadData(){
+		coffeeRepository.saveAll(List.of(
+				new Coffee("Cafe Cereza"),
+				new Coffee("Cafe Ganador"),
+				new Coffee("Cafe Lareno"),
+				new Coffee("Cafe Tres Pontas")
+		));
 	}
 }
